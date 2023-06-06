da1600c3e1cfc2b1fc7f52ac191c1abf2b7ec99a
Ankur Kumar Dwivedi <124455985+Ankurkumar45@users.noreply.github.com>
2023-06-06 22:53:14 +0530

    bcci

---
+++ leagues.php
@@ -0,0 +1,368 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+	<title>Indian cricket leagues</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+	<header>
+		<div class="homeMainDiv">
+			<div class="home_div">
+				<li><img src="BCCI.png" id="home_logo" class="home_head"></li>
+				<li>
+					<h3 id="home_headings" class="home_head">BCCI</h3>
+				</li>
+			</div>
+		</div>
+	</header>
+	<div class="navDiv">
+		<li><a href="Index.html" id="anc">HOME</a></li>
+		<li><a href="history.html" id="anc">HISTORY</a></li>
+		<li><a href="players.php" id="anc">PLAYERS</a></li>
+		<li><a href="wc.html" id="anc">WORLD CUPS</a></li>
+		<li><a href="contact.html" id="anc">CONTACT US</a></li>
+	</div>
+	<h1>Indian Cricket League</h1>
+	<p>The <b style="color:white">Indian Cricket League</b>(ICL) was a private cricket league funded by Zee Entertainment Enterprises that operated between 2007 and 2009 in India. Its two seasons included tournaments between four international teams (World XI, India, Pakistan and Bangladesh) and separate tournaments between nine domestic teams notionally located in major Indian cities as well as Lahore, Pakistan and Dhaka, Bangladesh.</p>
+
+	<table align="center" class="tab">
+		<tr>
+			<th id="icl_th" colspan="4" style="color:yellow">Indian Cricket League</th>
+		</tr>
+		<tr>
+			<th id="icl_th">Sports</th>
+			<td id="icl_td" colspan="3">Cricket</td>
+		</tr>
+		<tr>
+			<th id="icl_th">Founded</th>
+			<td id="icl_td" colspan="3">2007</td>
+		</tr>
+		<tr>
+			<th id="icl_th">Ceased</th>
+			<td id="icl_td" colspan="3">2009</td>
+		</tr>
+		<tr>
+			<th id="icl_th">Divisions</th>
+			<td id="icl_td" colspan="3">9</td>
+		</tr>
+		<tr>
+			<th id="icl_th">No. of teams</th>
+			<td id="icl_td" colspan="3">9 city teams, 4 international teams</td>
+		</tr>
+		<tr>
+			<th id="icl_th" rowspan="4">Country</th>
+			<td id="icl_td"><img src="Ind.jpg" id="image"></td>
+			<td id="icl_td">India</td>
+		</tr>
+		<tr>
+			<td id="icl_td"><img src="Pak.jpg" id="image"></td>
+			<td id="icl_td">Pakistan</td>
+		</tr>
+		<tr>
+			<td id="icl_td"><img src="Ban.jpg" id="image"></td>
+			<td id="icl_td">Bangladesh</td>
+		</tr>
+		<tr>
+			<td id="icl_td" colspan="3">World XI</td>
+		</tr>
+		<tr>
+			<th id="icl_th">Last champion(s)</th>
+			<td id="icl_td" colspan="3">Lahore Badshahs (2008/09)</td>
+		</tr>
+	</table>
+	<p>The matches were played in the Twenty20 format, and was the first domestic Twenty20 league in India. There was also a planned domestic 50-over tournament, but this did not happen. The ICL lacked the support of the Board of Control for Cricket in India and International Cricket Council, which placed it at a major disadvantage when compared to other cricket competitions.</p>
+	<p>
+		In 2008 the Board of Control for Cricket in India launched the their own Indian Premier League. ICL was an unsanctioned league. It is dubbed as "rebel league" by Indian media due to its fights with BCCI.
+	</p>
+	<p>
+		BCCI banned those who involved in ICL by the BCCI and other cricket authorities, led to the league's folding in 2009.
+	</p>
+	<h1 style="border-bottom: 2px solid yellow;">Indian Premier League</h1>
+	<p>The Indian Premier League (IPL) (also known as the TATA IPL for sponsorship reasons) is a men's Twenty20 (T20) cricket league held annually in India. It is contested by ten teams based in seven cities and three states of India. The league was founded by the Board of Control for Cricket in India (BCCI) in 2007. Brijesh Patel is the incumbent chairman of the IPL. Competition is usually held annually in summer (between March and May) and has an exclusive window in the ICC Future Tours Programme, meaning that international cricket does not take place during that the IPL season.</p>
+	<p>The IPL is the most popular cricket league in the world, and in 2014 it was ranked sixth by average attendance among all sports leagues.[needs update] In 2010, the IPL became the first sporting event in the world to be broadcast live on YouTube. The brand value of the IPL in 2022 was ₹90,038 crore (US$11 billion). According to BCCI, the 2015 IPL season contributed ₹1,150 crore (US$140 million) to the GDP of the Indian economy. In December 2022, the league became a decacorn valued at $10.9 billion registering a 75% growth in dollar terms since 2020 when it was valued at $6.2 billion, according to a report by consulting firm D & P Advisory.</p>
+	<p>There have been fifteen seasons of the tournament. The current title holders are the Gujarat Titans, winning the 2022 title in what was their first season.</p>
+	<div style="display: inline-block;">
+		<h4>Official website:</h4>
+	</div>
+	<div style="display: inline-block;"><a href="https://www.iplt20.com/" id="official_web" style="color: #e8d279; font-size: 15px; text-decoration: none;">IPLt20.com</a></div>
+	<h3>Prize money</h3>
+	<p>The 2019 season of the IPL offered a total prize money of ₹50 crore (equivalent to ₹53 crore or US$6.6 million in 2020), with the winning team netting ₹20 crore (equivalent to ₹21 crore or US$2.6 million in 2020). The first and second runners up received ₹12.5 crore (US$1.6 million) and ₹8.75 crore (US$1.1 million), respectively, with the fourth placed team also winning ₹8.75 crore (US$1.1 million). The other teams are not awarded any prize money. The IPL rules mandate that half of the prize money must be distributed among the players.</p>
+	<h2>Rules</h2>
+	<p>The IPL has some unique rules which the T20I format does not.</p>
+	<ul class="ipl_rules">
+		<li>Strategic Timeout - IPL games incorporate television timeouts. A penalty may be imposed if the umpires find teams misusing this privilege. Each team is given a 2:30 "strategic timeout" during each innings; one must be taken by the bowling team between the ends of the 6th and 9th overs, and one must be taken by the batting team between the ends of the 13th and 16th overs. To indicate the strategic timeout, the on-field umpire will raise his hand in the air and tap his wrist. This rule is borrowed from the NBA.</li><br>
+		<li>Since the 2018 season, the Decision Review System (DRS) is being used in all IPL matches, allowing each team two chances per innings to review an on-field umpire's decision. From the 2023 season, players can review wides and no-balls, which is not the case in other men's cricket tournaments.</li><br>
+		<li>If the bowling team doesn't complete its overs in allocated time, then they must place only 4 fielders outside of the 30-yard circle for the remainder of the innings.</li><br>
+		<li>Impact Player (Substitution) - Teams can use a substitute, which is termed as an "Impact player". For this substitution, the teams must handover a list of five players as probable substitutes to the match referee. The substitution can only be made before the start of an inning, when a wickets falls, when a batter retires, or at the end of an over. Teams can declare their playing XI and list of Impact Players after the toss, but all five Impact Players must be Indian. This rule is borrowed from football and the NBA. Both teams can introduce a substitute once per match. If a batsman gets out and the bowling team decides to make a substitution in the middle of the over, the substitute bowler cannot bowl the remaining balls of that over. The replaced player cannot take part in the match, even as a substitute fielder. Teams can introduce a foreign player as a substitute if they have included less than 4 foreign players in their playing XI.</li><br>
+		<li>There will be penalty of five runs if a fielder or wicketkeeper makes an unfair movement while the bowler is bowling and before ball reaches the batsman</li><br>
+		<li>After the fall of a wicket, the new batsman always will take the strike.</li><br>
+		<li>Teams can incorporate maximum 4 foreign players in the playing XI of 11.</li><br>
+		<li>Teams must include 25 players in their squad, and they can include only 8 foreign players.</li><br>
+	</ul>
+
+	<h3>Champions:-</h3>
+	<?php
+
+	require 'config2.php';
+
+	$qurey = mysqli_query($db, "SELECT * FROM indian_premier_league");
+
+	echo "<center><table><caption>Indian Premier League</caption>
+        <tr><th>Year</th><th>Champion Team</th><th>Captain</th><th>Coach</th></tr>";
+	while ($view = mysqli_fetch_array($qurey)) {
+		echo "<tr><td>
+            {$view['Year']}</td> <td>{$view['Champion']}</td> <td>{$view['Captain']}</td> <td>{$view['Coach']}</td></tr>";
+	}
+	echo "</table></center>";
+
+	echo "<style>
+        caption{
+            background-color: ;
+            font-weight: bolder;
+            font-size: 20px;
+			color: yellow;
+        }
+        table,th,td{
+            font-size: 15px;
+            border: 2px solid wheat;
+            border-collapse: collapse;
+            padding:10px;
+            font-family: Arial, Helvetica, sans-serif;
+        }
+    </style>";
+	?>
+
+	<h2 align="center" style="font-size: 30px">Awards</h2>
+	<h4>Orange Cap</h4>
+	<p>The Orange Cap is awarded to the top run-scorer in the IPL during a season. It is an ongoing competition with the leader wearing the cap throughout the tournament until the final game, with the eventual winner keeping the cap for the season. Latest winner – Jos Buttler – 863 Runs (2022).</p>
+	<h4>Purple Cap</h4>
+	<p>The Purple Cap is awarded to the top wicket-taker in the IPL during a season. It is an ongoing competition with the leader wearing the cap throughout the tournament until the final game, with the eventual winner keeping the cap for the season. Latest winner – Yuzvendra Chahal – 27 wickets (2022).</p>
+	<h4>Most Valuable Player(MVP)</h4>
+	<p>The award was called the "man of the tournament" until the 2012 season. The IPL introduced the Most Valuable Player rating system in 2013, the leader of which would be named the "Most Valuable Player" at the end of the season. Latest winner – Jos Buttler (2022).</p>
+	<h4>Fairplay Award</h4>
+	<p>The Fair Play Award is given after each season to the team with the best record of fair play. The winner is decided on the basis of the points the umpires give to the teams. After each match, the two on-field umpires, and the third umpire, scores the performance of both teams. Latest winners –Rajasthan Royals.</p>
+	<h4>Emerging Player Award</h4>
+	<p>The award was presented for the "best under-19 player" in 2008 and "best under-23 player" in 2009 and 2010, being called "Under-23 Success of the Tournament". In 2011 and 2012, the award was known as "Rising Star of the Year", while, in 2013, it was called "Best Young Player of the Season". Since 2014, the award has been called the Emerging Player of the Year. In 2016, Mustafizur Rahman of Bangladesh was the first and only foreign player to date to win the Emerging Player of the Year award. Latest winner – Umran Malik (2022).</p>
+	<h4>Maximum Sixes Award</h4>
+	<p>The award for the most sixes in a season, currently known as Unacademy Let's Crack It Sixes Award for sponsorship reasons, is presented to the batsman who hits the most sixes in a season of the IPL. English batsman Jos Buttler of the Rajasthan Royals won this award in 2022 with 45 sixes in 17 innings.</p>
+
+	<h1 style="border-bottom: 2px solid yellow;">Women's Premier League (cricket)</h1>
+	<h3>Organisation</h3>
+	<p>The league's structure is based on the structure of the IPL.
+
+		Initially there are five teams, with sides playing against each other in a double round robin format, and the three teams finishing with the most points entering the playoff stages of the competition. The Board plans to increase the number of matches and franchises in future seasons if the league is a success.
+
+		The first season of the league took place from 4 March to 26 March 2023, and featured 22 matches, all held at Brabourne Stadium and DY Patil Stadium in Mumbai. Tickets were made available free to women during the first season.
+
+		The league's mascot, Shakti, is a tigress wearing a sky blue cricket uniform.</p>
+	<h3>Franchises</h3>
+	<p>Investors brought the initial franchise rights in January 2023 through a closed bidding process, raising a total of ₹4,669 crore (US$580 million).</p>
+	<p>A number of companies responded to the sale of franchise rights which were sold for five years, from 2023 to 2027. Adani Group won the rights to the Ahmedabad franchise for ₹1,289 crore (US$160 million), Indiawin Sports Pvt Ltd, part of Reliance Industries, won the Mumbai franchise for ₹912.99 crore (US$110 million), GMR–JSW Cricket Pvt Ltd won the Delhi franchise for ₹810 crore (US$100 million), Capri Global holdings won the Lucknow franchise for ₹757 crore (US$95 million), and Royal Challengers Sports Pvt Ltd, a subsidiary of alcohol manufacturing company Diageo, won the Bangalore franchise for ₹901 crore (US$110 million).</p>
+
+	<h3>Champions:-</h3>
+	<?php
+
+	require 'config2.php';
+
+	$qurey = mysqli_query($db, "SELECT * FROM women_premier_league");
+
+	echo "<center><table><caption>Women Premier League</caption>
+	<tr><th>Year</th><th>Champion Team</th><th>Captain</th><th>Coach</th></tr>";
+	while ($view = mysqli_fetch_array($qurey)) {
+		echo "<tr><td>
+		{$view['Year']}</td> <td>{$view['Champion']}</td> <td>{$view['Captain']}</td> <td>{$view['Coach']}</td></tr>";
+	}
+	echo "</table></center>";
+
+	echo "<style>
+	caption{
+		background-color: ;
+		font-weight: bolder;
+		font-size: 20px;
+		color: yellow;
+	}
+	table,th,td{
+		font-size: 15px;
+		border: 2px solid wheat;
+		border-collapse: collapse;
+		padding:10px;
+		font-family: Arial, Helvetica, sans-serif;
+	}
+</style>";
+	?>
+
+
+	<footer class="footerDivMain">
+		<div class="footerDiv">
+			<div class="footerDiv1">
+				<h4 id="homeh4">CONTACT US</h4>
+				<p>
+					<center style="color: rgb(150, 142, 142);" id="homeCenter">4th Floor, Cricket Centre <br>
+						Wankhede Stadium <br>
+						‘D’ Road, Churchgate <br>
+						Mumbai- 400020 <br>
+						India. <br>
+						E-mail: office@bcci.tv</center>
+				</p>
+			</div>
+			<div class="footerDiv2">
+				<center>
+					<div class="footerDiv2Child1">
+						<h4 id="homeh4">MATCHES</h4>
+						<p>
+							<center><a href="https://www.bcci.tv/domestic/fixtures" id="footeranc">Domestic</a></center>
+						</p>
+						<p>
+							<center><a href="https://www.bcci.tv/international/fixtures" id="footeranc">International</a>
+							</center>
+						</p>
+					</div>
+					<div class="footerDiv2Child2">
+						<h4 id="homeh4">PLAYERS</h4>
+						<p>
+							<center><a href="players.html" id="footeranc">Men</a></center>
+						</p>
+						<p>
+							<center><a href="women.html" id="footeranc">Women</a></center>
+						</p>
+					</div>
+				</center>
+			</div>
+			<div class="footerDiv3">
+				<h4 id="homeh4">VIDEOS</h4>
+				<p>
+					<center><a href="https://www.bcci.tv/videos/Latest" id="footeranc">Latest</a></center>
+				</p>
+				<p>
+					<center><a href="https://www.bcci.tv/videos/highlights" id="footeranc">Highlights</a></center>
+				</p>
+				<p>
+					<center><a href="https://www.bcci.tv/videos/features-and-interviews" id="footeranc">Features and
+							Interviews</a></center>
+				</p>
+				<p>
+					<center><a href="https://www.bcci.tv/videos/press-conference" id="footeranc">Press Conferences</a>
+					</center>
+				</p>
+			</div>
+			<div class="footerDiv4">
+				<h4 id="homeh4">NEWS</h4>
+				<p>
+					<center><a href="https://www.bcci.tv/articles" id="footeranc">Latest</a></center>
+				</p>
+				<p>
+					<center><a href="https://www.bcci.tv/news/bcci-news" id="footeranc">BCCI News</a></center>
+				</p>
+			</div>
+			<div class="footerDiv5">
+				<h4 id="homeh4">MORE</h4>
+				<p>
+					<center><a href="https://www.iplt20.com/" id="footeranc">IPL</a></center>
+				</p>
+				<p>
+					<center><a href="https://www.bcci.tv/venues" id="footeranc">Vanue</a></center>
+				</p>
+			</div>
+		</div>
+		<footer class="homefooter">
+			<div>
+				<span>T +91 22 - 2289 8800
+
+					F 022 - 2289 8801</span>
+			</div>
+			<div>
+				<span>Copyrights BCCI 2021 , All Rights Reserved.</span>
+			</div>
+			<div>
+				<span><a href="https://www.bcci.tv/about/terms-of-use" id="tc">Terms and Conditions</a></span>
+			</div>
+			<div>
+				<span><a href="https://www.bcci.tv/about/privacy-policy" id="tc">Privacy Policy</a></span>
+			</div>
+			<div><span>
+					<address>Created by: Ankur Dwivedi</address>
+				</span></div>
+		</footer>
+	</footer>
+
+</body>
+
+<style>
+	.leagueTable,
+	th,
+	td {
+		border: 2px solid rgb(246, 123, 22);
+		border-collapse: collapse;
+	}
+
+	.leagueTable tr td a {
+		text-decoration: none;
+		color: white;
+	}
+
+	.leagueTable tr td a:hover {
+		background-color: aqua;
+		text-decoration: underline;
+		color: black;
+		border-radius: 50%;
+		padding: 7px;
+	}
+
+	.leagueTable tr td a:active {
+		background-color: red;
+		text-decoration: underline;
+		color: white;
+		border-radius: 50%;
+		padding: 7px;
+	}
+
+	.ipl_rules {
+		color: white;
+		text-align: justify;
+	}
+
+	#image {
+		width: 20px;
+		height: 20px;
+	}
+
+	.tab,
+	#icl_th,
+	#icl_td {
+		color: #d9b2bd;
+		border: 3px solid #bacae3;
+		border-collapse: collapse;
+		padding: 5px;
+	}
+
+	h1 {
+		font-weight: bolder;
+		font-size: 30px;
+		color: yellow;
+	}
+
+	@media screen and (max-width: 600px) {
+
+		.ipl_teams th,
+		td {
+			padding: 7px;
+			font-size: 7px;
+		}
+
+		.leagueTable th,
+		td {
+			padding: 7px;
+			font-size: 7px;
+		}
+
+		.ipl_teams,
+		.leagueTable {
+			width: 300px;
+		}
+	}
+</style>
+
+</html>
---
+++ login-w-2.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>update</title>
+</head>
+
+<body>
+    <div class="container">
+        <h3>Update</h3>
+        <form action="backend-w-2.php" method="post" enctype="multipart/form-data">
+            <input type="text" name="Name" placeholder="Enter Player's Name" required><br><br>
+            <input type="text" name="Role" placeholder="Enter Player's Role" required><br><br>
+            <input type="file" name="uploaded" required><br><br>
+            <input type="submit" name="smt">
+        </form>
+    </div>
+    <style>
+        h3 {
+            border-bottom: 3px solid black;
+        }
+
+        .container {
+            background-color: wheat;
+            padding: 20px;
+        }
+
+        body {
+            background-color: aqua;
+        }
+    </style>
+
+</body>
+
+</html>
+
+<?php
+$a = mysqli_connect('localhost', 'root', '', 'user_registration');
+
+$E = $_POST['Email'];
+$P = $_POST['Password'];
+
+$qurey = mysqli_query($a, "SELECT * FROM registration_form WHERE Email='$E' and Password='$P'");
+if (mysqli_num_rows($qurey) > 0) {
+    echo "<script>
+            alert('Login Successfull...');
+        </script>";
+} else {
+    header('location:loginw-A.html');
+    echo "Please enter correct password";
+}
+?>
